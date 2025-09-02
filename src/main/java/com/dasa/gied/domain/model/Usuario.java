34fe179c286bf21fe20bfe4bd38204d9f767bb75
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-02 16:33:40 -0300

    Usuario

--- src/main/java/com/dasa/gied/domain/model/Usuario.java
+++ src/main/java/com/dasa/gied/domain/model/Usuario.java
@@ -1,5 +1,9 @@
 package com.dasa.gied.domain.model;
 
+import com.dasa.gied.domain.enums.TipoUsuario;
+
+import java.util.Objects;
+
 public class Usuario {
     private Long id;
     private String nome;
@@ -7,5 +11,64 @@ public class Usuario {
     private String senhaHash;
     private TipoUsuario tipo;
 
+    public Usuario(Long id, String nome, String login, String senhaHash, TipoUsuario tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senhaHash = senhaHash;
+        this.tipo = tipo;
+    }
+    public Usuario(){}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
