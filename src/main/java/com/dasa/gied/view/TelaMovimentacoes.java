2b3a388eca3bb7b8526456821f8cfd41becadc54
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-09 19:46:55 -0300

    Estruturando Tela Movimentacoes

---
+++ src/main/java/com/dasa/gied/view/TelaMovimentacoes.java
@@ -0,0 +1,9 @@
+package com.dasa.gied.view;
+
+import javax.swing.*;
+
+public class TelaMovimentacoes {
+    private JPanel panel1;
+    private JButton registrarSaídaButton;
+    private JButton registrarEntradaButton;
+}
