bfbc03926e6ba2a0319d8210d39bb13eb1240199
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-09 12:45:11 -0300

    estruturando a tela menu

--- src/main/java/com/dasa/gied/view/TelaMenu.java
+++ src/main/java/com/dasa/gied/view/TelaMenu.java
@@ -3,4 +3,13 @@ package com.dasa.gied.view;
 import javax.swing.*;
 
 public class TelaMenu {
+    private JButton consultarEstoqueButton;
+    private JButton gerenciarUsuariosButton;
+    private JButton movimentacoesButton;
+    private JButton relatoriosButton;
+    private JButton sairButton;
+    private JButton alterarMinhaSenhaButton;
+    private JPanel PainelOp;
+    private JPanel PainelSair;
+    private JLabel tituloMenu;
 }
