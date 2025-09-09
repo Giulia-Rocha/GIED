d67bb35a50ebeec53955f3b47869652e94084227
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-09 20:34:45 -0300

    Estruturando Tela Gerenciar Usuario

---
+++ src/main/java/com/dasa/gied/view/TelaGerenciarUsuario.java
@@ -0,0 +1,10 @@
+package com.dasa.gied.view;
+
+import javax.swing.*;
+
+public class TelaGerenciarUsuario {
+    private JPanel panel1;
+    private JButton registrarUsuárioButton;
+    private JButton deletarUsuárioButton;
+    private JButton buscarPorIDButton;
+}
