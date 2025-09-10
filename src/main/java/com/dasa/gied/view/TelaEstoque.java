b5a55118b09be0eaea3beed78861bdbfa587ad6c
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-09-10 11:42:53 -0300

    Tela Estoque

---
+++ src/main/java/com/dasa/gied/view/TelaEstoque.java
@@ -0,0 +1,11 @@
+package com.dasa.gied.view;
+
+import javax.swing.*;
+
+public class TelaEstoque {
+    private JPanel panel1;
+    private JButton estoqueTodoButton;
+    private JButton estoqueBaixoButton;
+    private JButton voltarButton;
+    private JButton porIDDoItemButton;
+}
